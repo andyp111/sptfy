cdbad8ad072b973fea2c0946c36c3c2b10cd75ad
Andy Pham <andypham@Andys-MacBook-Pro.local>
2021-01-30 16:20:46 -0800

    using oauth bridge to connect

--- server/router.js
+++ server/router.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller/controller.js');
 
-router
-  .route('/login')
-  .get(controller.getLogin)
+// router
+//   .route('/login')
+//   .get(controller.getLogin)
 
-router
-  .route('/callback')
-  .get(controller.afterLogin)
+// router
+//   .route('/callback')
+//   .get(controller.afterLogin)
 
 module.exports = router;
