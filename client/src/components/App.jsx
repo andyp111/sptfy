22d174ff999473f9d2df688f498233b551165f7d
Andy Pham <andypham@Andys-MacBook-Pro.local>
2021-02-05 13:39:19 -0800

    rerender playlist

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -56,7 +56,13 @@ class App extends React.Component {
         {this.state.username ?
           <div>
             <h1>{this.state.username}'s Dashboard</h1>
-            <Playlists names={this.state.playlists} />
+            {this.state.playlists.map((name, index) => {
+              return (
+                <div key={index}>
+                  <Playlists playlist={name} />
+                </div>
+              )
+            })}
             </div>
             : <button onClick={() => window.location = 'http://vpz-sptfy-backend.herokuapp.com/login'}>Sign in</button>
         }
--- client/src/components/Playlists.jsx
+++ client/src/components/Playlists.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 
-const Playlists = (props) => {
-    console.log(props.names)
-    return (
-        <div>
-            {props.names.map((name, index) => {
-                return (
-                    <div key={index}>
-                       <img src={name.image} />
-                        {name.name}
-                    </div>
-                )
-            })}
-        </div>
-    )
+
+
+class Playlists extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            songList: [],
+            clicked: false
+        }
+
+        this.onImageClick = this.onImageClick.bind(this);
+    };
+
+
+    onImageClick() {
+        this.setState({
+            clicked: !this.state.clicked
+        }, () => console.log(this.state.clicked))
+    }
+
+    render() {
+        return (
+            <div>
+                <img onClick={this.onImageClick} src={this.props.playlist.image} />
+            </div>
+        )
+    }
 }
 
 export default Playlists
